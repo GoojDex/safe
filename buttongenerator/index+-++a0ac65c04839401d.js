99839bac0ef44b8c894bea4957cbef74d8915866
GoojDex <114043672+GoojDex@users.noreply.github.com>
2023-01-30 17:26:14 +0530

    Update and rename index+-+a0ac65c04839401d.js to index+-++a0ac65c04839401d.js

--- buttongenerator/index+-++a0ac65c04839401d.js
+++ buttongenerator/index+-++a0ac65c04839401d.js
@@ -794,10 +794,10 @@
                                         height: 25,
                                         width: 25
                                     },
-                                    url: "https://facebook.com/#"
-                                }), "\xa0\xa0\xa0Copyright\xa0", (0, r.jsx)("a", {
-                                    href: "https://goojdextools.blogspot.com",
-                                    children: "Premium CSS Buttons"
+                                    url: "https://facebook.com/GoojDex"
+                                }), "\xa0\xa0\xa0Copyright &copy;\xa0", (0, r.jsx)("a", {
+                                    href: "https://premiumcssbuttongenerator.blogspot.com/",
+                                    children: "Premium CSS Buttons Generator"
                                 }), "\xa0Powered By\xa0", (0, r.jsx)("a", {
                                     href: "https://devchandel-goojdex.blogspot.com",
                                     target: "_blank",
